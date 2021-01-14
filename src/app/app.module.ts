ab0ae093785a18ed4e02b7e43edf9db41dcf8782
ahmedharby <ahmedharby9@gmail.com>
2021-01-14 20:50:08 +0200

    Configur All dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { UsersComponent } from './users/users.component';
+import { SingleUserViewComponent } from './single-user-view/single-user-view.component';
+import { SingleUserAddComponent } from './single-user-add/single-user-add.component';
+import { SingleUserEditComponent } from './single-user-edit/single-user-edit.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    UsersComponent,
+    SingleUserViewComponent,
+    SingleUserAddComponent,
+    SingleUserEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
