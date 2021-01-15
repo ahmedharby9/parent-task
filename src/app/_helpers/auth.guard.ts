5917d6d82f8456b22061e1a32d25b9cb093459e8
ahmedharby <ahmedharby9@gmail.com>
2021-01-15 20:03:33 +0200

    configure auth guard

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {CallApiService} from '../services/call-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private callApi: CallApiService) {
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot,
+    ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.callApi.checkToken();
   }
-  
+
 }
