709ff82ec740857d930a2837fdc90857460af795
ahmedharby <ahmedharby9@gmail.com>
2021-01-16 00:32:08 +0200

    change header based on current URL

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public isLoggedIn: boolean;
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.isLoggedIn = false;
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log(localStorage.getItem('_token'));
+        if (event.urlAfterRedirects.includes('login') || event.url.includes('login')) {
+          this.isLoggedIn = false;
+        } else {
+          this.isLoggedIn = true;
+        }
+      }
+
+    });
   }
 
 }
