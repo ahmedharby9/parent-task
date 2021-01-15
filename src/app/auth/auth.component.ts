ffc2bea18bac3dc7997512a7d3256bee2fd6731f
ahmedharby <ahmedharby9@gmail.com>
2021-01-15 19:58:52 +0200

    Integrate the login page with the server

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CallApiService} from '../services/call-api.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  public userLoginInfo: FormGroup;
 
-  constructor() { }
+  constructor(private callApi: CallApiService, private fb: FormBuilder, private router: Router, private toastr: ToastrService) {
+    this.userLoginInfo = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+    this.callApi.setLogout();
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    if (this.userLoginInfo.invalid) {
+      return;
+    }
+    this.callApi.setLogin(this.userLoginInfo.value).subscribe((res) => {
+      localStorage.setItem('_token', res.token);
+      this.toastr.success('ddfdfefe', 'Server Error!');
+      setTimeout(() => {
+        this.router.navigateByUrl('/users');
+      }, 1000);
+
+    });
   }
 
 }
