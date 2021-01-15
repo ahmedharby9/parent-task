f7e24719ed790e42f0b775f329918e5d25879f3d
ahmedharby <ahmedharby9@gmail.com>
2021-01-15 20:02:20 +0200

    configer an interceptor for http calling

--- src/app/_helpers/http-config.interceptor.ts
+++ src/app/_helpers/http-config.interceptor.ts
@@ -1,18 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse, HttpResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
+import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class HttpConfigInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private toastr: ToastrService) {
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token: string = localStorage.getItem('token');
+    if (token) {
+      request = request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + token),
+      });
+    }
+    if (!request.headers.has('Content-Type')) {
+      request = request.clone({
+        headers: request.headers.set('Content-Type', 'application/json'),
+      });
+    }
+    request = request.clone({
+      headers: request.headers.set('Accept', 'application/json'),
+    });
+    return next.handle(request).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          return event;
+        }
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.toastr.error(error.error.error, 'Server Error!');
+        return throwError(error);
+      })
+    );
   }
 }
