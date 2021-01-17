24da27ae92a6e3333d0ca2043c3a23359baacec2
ahmedharby <ahmedharby9@gmail.com>
2021-01-17 20:26:43 +0200

    bind data in user screen and test logic

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,25 +1,106 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { UsersComponent } from './users.component';
+import {UsersComponent} from './users.component';
+import {DOMHelper} from '../../testing/dom-helper';
+import {Observable, of} from 'rxjs';
+import {RouterTestingModule} from '@angular/router/testing';
+import {CallApiService} from '../services/call-api.service';
+import {ToastrService} from 'ngx-toastr';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {User} from '../models/user';
+import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
+import {SingleUserEditComponent} from '../single-user-edit/single-user-edit.component';
+import {SingleUserViewComponent} from '../single-user-view/single-user-view.component';
+
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
   let fixture: ComponentFixture<UsersComponent>;
+  let dh: DOMHelper<UsersComponent>;
+  let userServiceMock: any;
+  let toastServiceMock: any;
 
   beforeEach(async () => {
+    userServiceMock = jasmine.createSpyObj('CallApiService', ['getUsersList']);
+    toastServiceMock = jasmine.createSpyObj('ToastrService', ['success']);
+
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
+      declarations: [
+        UsersComponent,
+        ConfirmDialogComponent,
+        SingleUserEditComponent,
+        SingleUserViewComponent
+      ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: ToastrService, useValue: toastServiceMock},
+
+        {provide: CallApiService, useValue: userServiceMock}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UsersComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    dh = new DOMHelper<UsersComponent>(fixture);
   });
 
-  it('should create', () => {
+  it('should created', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Nav tag content', () => {
+    it('should contain of title', () => {
+      expect(dh.singleText('h1')).toBe('Users List');
+    });
+    it('should contain of Home root', () => {
+      expect(dh.countText('li', 'Home')).toBe(1);
+    });
+    it('should contain of Dashboard', () => {
+      expect(dh.countText('li', 'Dashboard')).toBe(1);
+    });
+    it('should contain of button New User', () => {
+      expect(dh.countText('button', 'New User')).toBe(1);
+    });
+  });
+
+  describe('Users table loading and binding ', () => {
+
+    it('Should show One Unordered List Item', () => {
+      expect(dh.count('table')).toBe(1);
+    });
+    it('should contain of empty list', () => {
+      expect(dh.count('td')).toBe(0);
+    });
+    it('should contain at least one user in list', () => {
+
+    });
+  });
+
+
+  xdescribe('login process and authenticate user', () => {
+
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it('should clear token', () => {
+      component.ngOnInit();
+      let token = localStorage.getItem('_token');
+      expect(token).toBeNull();
+    });
+
+    it('should call onSubmit method', () => {
+      dh.clickButton('Login');
+    });
+
+  });
+
 });
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {CallApiService} from '../services/call-api.service';
 import {User} from '../models/user';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-users',
@@ -13,24 +14,18 @@ export class UsersComponent implements OnInit {
   public isLoading: boolean;
   public currentSelectedUser: User;
   public viewUserSide: boolean;
+  public deleteConfirmMsg: string;
 
-  constructor(private callApi: CallApiService) {
+  constructor(private callApi: CallApiService, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.callApi.getUsersList(`page=${this.paging.page}&per_page=${this.paging.per_page}`).subscribe((res) => {
-      this.isLoading = false;
-      this.userListInfo = res.data;
-      this.paging.page = res.page;
-      this.paging.per_page = res.per_page;
-      this.paging.total = res.total;
-    });
+    this.onLoadingAllUsers(`page=${this.paging.page}&per_page=${this.paging.per_page}`);
   }
 
   // tslint:disable-next-line:typedef
   onScroll(event) {
-    console.log(Math.ceil(this.paging.total / this.paging.per_page));
     if (this.paging.page < (Math.ceil(this.paging.total / this.paging.per_page))) {
       this.isLoading = true;
       this.callApi.getUsersList(`page=${this.paging.page + 1}&per_page=${this.paging.per_page}`).subscribe((res) => {
@@ -41,7 +36,33 @@ export class UsersComponent implements OnInit {
         this.paging.total = res.total;
       });
     }
-
   }
 
+  onClickDelete(user): void {
+    this.currentSelectedUser = user;
+    this.deleteConfirmMsg = `<p>Are you sure you want to delete</p><p class="font-weight-bolder">${user.first_name} ${user.last_name}</p> `;
+  }
+
+  onApproveDeleteUser(event): void {
+    if (event) {
+      this.callApi.deleteUser(this.currentSelectedUser.id).subscribe((res) => {
+        this.toastr.success('User has been deleted successfully', 'Deleted Message!');
+        this.onLoadingAllUsers(`page=${1}&per_page=${this.paging.per_page * this.paging.page}`);
+      });
+    }
+  }
+
+  onLoadingAllUsers(arg?): void {
+    this.callApi.getUsersList(arg).subscribe((res) => {
+      this.isLoading = false;
+      this.userListInfo = res.data;
+      this.paging.page = res.page;
+      this.paging.per_page = res.per_page;
+      this.paging.total = res.total;
+    });
+  }
+
+  onEditOrAddUser(): void {
+    this.onLoadingAllUsers(`page=${this.paging.page}&per_page=${this.paging.per_page}`);
+  }
 }
