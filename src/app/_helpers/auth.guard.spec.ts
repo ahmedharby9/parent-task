43a0486ae0cc51bd7ede25797c9c99444577a741
ahmedharby <ahmedharby9@gmail.com>
2021-01-17 20:31:36 +0200

    no changes

--- src/app/_helpers/auth.guard.spec.ts
+++ src/app/_helpers/auth.guard.spec.ts
@@ -1,12 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { AuthGuard } from './auth.guard';
+import {AuthGuard} from './auth.guard';
+import {CallApiService} from '../services/call-api.service';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        CallApiService
+      ]
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
