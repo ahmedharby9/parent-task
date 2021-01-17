df0bdfee3a4a04f95345198dcd0e3c9fbaaf55bd
ahmedharby <ahmedharby9@gmail.com>
2021-01-17 20:30:51 +0200

    change buttons style and set it in global style file

--- src/app/single-user-edit/single-user-edit.component.spec.ts
+++ src/app/single-user-edit/single-user-edit.component.spec.ts
@@ -1,25 +1,84 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SingleUserEditComponent } from './single-user-edit.component';
+import {SingleUserEditComponent} from './single-user-edit.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ToastrModule, ToastrService} from 'ngx-toastr';
+import {CallApiService} from '../services/call-api.service';
+import {DOMHelper} from '../../testing/dom-helper';
+import {User} from '../models/user';
+import {SafePipe} from '../pipes/safe.pipe';
 
 describe('SingleUserEditComponent', () => {
   let component: SingleUserEditComponent;
   let fixture: ComponentFixture<SingleUserEditComponent>;
+  let dh: DOMHelper<SingleUserEditComponent>;
+  let userServiceMock: any;
+  let toastServiceMock: any;
 
   beforeEach(async () => {
+    userServiceMock = jasmine.createSpyObj('CallApiService', ['getOneUser']);
+    toastServiceMock = jasmine.createSpyObj('ToastrService', ['success']);
+
     await TestBed.configureTestingModule({
-      declarations: [ SingleUserEditComponent ]
+      declarations: [
+        SingleUserEditComponent,
+        SafePipe
+      ],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: ToastrService, useValue: toastServiceMock},
+        {provide: CallApiService, useValue: userServiceMock}
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SingleUserEditComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    dh = new DOMHelper<SingleUserEditComponent>(fixture);
   });
 
-  it('should create', () => {
+  it('should created', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Create mode content', () => {
+    let helper: Helper;
+    beforeEach(() => {
+      helper = new Helper();
+      fixture.detectChanges();
+    });
+
+    it('should not contain of img ', () => {
+      component.data = helper.getUsers();
+      fixture.detectChanges();
+      expect(dh.count('img')).toBe(1);
+    });
+    it('should return call onSubmitUpdate and create new one ', () => {
+      spyOn(component.onCallBack, 'emit');
+      spyOn(component, 'onSubmitCreate');
+      component.data = {};
+      component.userInfo.setValue({name: 'null', job: 'kmk'});
+      fixture.detectChanges();
+      dh.clickButton('Add');
+      expect(component.onSubmitCreate).toHaveBeenCalled();
+    });
+  });
 });
+
+class Helper {
+  users: User [] = [];
+
+  getUsers(): any {
+    return {id: 1, first_name: 'user', last_name: 'last', avatar: 'http://img'};
+  }
+}
+
--- src/app/single-user-edit/single-user-edit.component.ts
+++ src/app/single-user-edit/single-user-edit.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {User} from '../models/user';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CallApiService} from '../services/call-api.service';
@@ -11,8 +11,9 @@ import {ToastrService} from 'ngx-toastr';
 })
 export class SingleUserEditComponent implements OnInit, OnChanges {
   @Input() data: User;
-  public userInfo: FormGroup;
+  @Output() onCallBack: EventEmitter<any> = new EventEmitter<any>();
   @ViewChild('closebutton') closeButton;
+  public userInfo: FormGroup;
 
   constructor(private fb: FormBuilder, private callApi: CallApiService, private toast: ToastrService) {
     this.userInfo = this.fb.group({
@@ -28,6 +29,7 @@ export class SingleUserEditComponent implements OnInit, OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     if (changes && changes['data'] && changes['data'].currentValue) {
       this.data = changes['data'].currentValue;
+      this.userInfo.reset();
       this.userInfo.setValue({name: this.data.first_name ?? '', job: ''});
     }
   }
@@ -39,6 +41,8 @@ export class SingleUserEditComponent implements OnInit, OnChanges {
     }
     this.callApi.putUpdateUser(this.data.id, this.userInfo.value).subscribe((res) => {
       this.toast.success('User data has been updated successfully', 'Updated Message!');
+      this.onCallBack.emit(true);
+      console.log(this.closeButton.nativeElement);
       this.closeButton.nativeElement.click();
     });
   }
@@ -50,8 +54,10 @@ export class SingleUserEditComponent implements OnInit, OnChanges {
       return;
     }
     this.callApi.postNewUser(this.userInfo.value).subscribe((res) => {
-      this.toast.success('User data has been created successfully', 'Create Message!');
-      this.closeButton.nativeElement.click();
-    });
+        this.toast.success('User data has been created successfully', 'Create Message!');
+        this.onCallBack.emit(true);
+        this.closeButton.nativeElement.click();
+      }
+    );
   }
 }
