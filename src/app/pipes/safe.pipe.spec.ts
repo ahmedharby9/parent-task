08ee8d76d9fdbd30d5657c52a50abaced65401a4
ahmedharby <ahmedharby9@gmail.com>
2021-01-17 20:32:42 +0200

    no changes

--- src/app/pipes/safe.pipe.spec.ts
+++ src/app/pipes/safe.pipe.spec.ts
@@ -1,8 +1,10 @@
-import { SafePipe } from './safe.pipe';
+import {SafePipe} from './safe.pipe';
 
 describe('SafePipe', () => {
+
   it('create an instance', () => {
     const pipe = new SafePipe();
     expect(pipe).toBeTruthy();
   });
+
 });
--- src/app/pipes/safe.pipe.ts
+++ src/app/pipes/safe.pipe.ts
@@ -5,7 +5,7 @@ import {DomSanitizer} from '@angular/platform-browser';
   name: 'safe'
 })
 export class SafePipe implements PipeTransform {
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(public sanitizer?: DomSanitizer) {
   }
 
   // tslint:disable-next-line:typedef
